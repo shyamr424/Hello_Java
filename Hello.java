ca8a23aa6a8cd303833bea7c29cb0be564464f3a
shyam <116052972+shyamr424@users.noreply.github.com>
2022-10-18 01:54:49 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-public class Hello{
-  public static void main(String[] args){
+public class Hello {
+  public static void main(String[] args) {
       //Prints "Hello, Worlds" in the terminal window.
-      System.out.println("Hellow, World");
+      System.out.println("Hello, World");
       }
   } 
